afdc92868eef9f78bd05e9136530d4beccd0c64c
rstriquer <rstriquer@gmail.com>
2023-10-28 17:05:57 -0300

    feat: configure swagger on the application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,57 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: process.env.SWAGGER_API_VERSION || '1.0',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(
+      process.env.SWAGGER_DESCRIPTION +
+        '<br />This file can be downloaded on the "api-json" as a json file.',
+    )
+    .setVersion(process.env.SWAGGER_API_VERSION || '1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+
   await app.listen(3000);
+
+  const availableRoutes: [] = router.stack
+    .map((layer) => {
+      if (layer.route) {
+        return {
+          route: {
+            path: layer.route?.path,
+            method: layer.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter((item) => item !== undefined);
+  console.log('Available routes: ', availableRoutes);
 }
 bootstrap();
