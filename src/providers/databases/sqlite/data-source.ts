12369ff7a4b947cf17277d3a1301b6b9023c9765
rstriquer <rstriquer@gmail.com>
2023-10-29 19:17:45 -0300

    feat: Add SQLite3 data provider (DataSource)

--- src/providers/databases/sqlite/data-source.ts
+++ src/providers/databases/sqlite/data-source.ts
@@ -6,11 +6,12 @@ export const dataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
   database: process.env.DB_DATABASE,
   synchronize: true,
-  entities: ['dist/entities/*.entity.js'],
+  entities: ['dist/src/entities/*.entity.js'],
   migrationsTableName: 'migrations',
   migrations: ['dist/database/migrations/*.js'],
   migrationsRun: false,
-}
+  logging: true,
+};
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
