f565295a4a5c74112d2af42cbc4039f65775e321
rstriquer <rstriquer@gmail.com>
2023-10-30 17:23:43 -0300

    feat: Activate movements module & auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
+import { AuthMiddleware } from './common';
 import { DataSource } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { HealthCheckModule } from './health-check';
 import { AccountModule } from './accounts';
+import { MovementModule } from './movements';
 import { dataSourceOptions } from './providers/databases/sqlite';
 
 @Module({
@@ -12,9 +14,14 @@ import { dataSourceOptions } from './providers/databases/sqlite';
       name: 'default',
     }),
     AccountModule,
+    MovementModule,
     HealthCheckModule,
   ],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   constructor(private dataSource: DataSource) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('v1/movements');
+  }
 }
