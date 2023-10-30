538a79bd85e1da6032605a3263356001803a68f3
rstriquer <rstriquer@gmail.com>
2023-10-30 12:31:23 -0300

    fix(accounts): Change the module directory name

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { HealthCheckModule } from './health-check';
-import { AccountModule } from './account';
+import { AccountModule } from './accounts';
 import { dataSourceOptions } from './providers/databases/sqlite';
 
 @Module({
